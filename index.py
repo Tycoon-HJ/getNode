9e3cbe6be9ab3235338f7211902cfe045748975e
yinhaijun <2523284817@qq.com>
2022-07-06 21:07:08 +0800

    优化程序，避免今天没有新的节点，去最后一天去获取节点

--- index.py
+++ index.py
@@ -4,19 +4,24 @@ from lxml import etree
 import datetime
 import time
 
+
 # 在当前py文件的目录中创建文件《节点.txt》
-file = open("节点.txt","w")
+file = open("节点.txt", "w")
 
 # 格式化时间
-def ftime(f = ''):
-    return time.strftime('%Y%m%d', time.localtime()) 
+
+
+def ftime(f=''):
+    return time.strftime('%Y%m%d', time.localtime())
+
 
 # 模拟浏览器请求
-handlers = {'ser-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36'}
+handlers = {
+    'ser-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36'}
 
 # 要爬去的目标网址
-baseUrl = "https://www.cfmem.com";
-res = requests.get(baseUrl,handlers)
+baseUrl = "https://www.cfmem.com"
+res = requests.get(baseUrl, handlers)
 content = res.content
 html = etree.HTML(content)
 
@@ -35,25 +40,47 @@ count = urlParm[urlParm.find('点')+1:urlParm.find('条')]
 # 4K
 otherParm = urlParm[urlParm.find('清')+1:urlParm.find('上')]
 
+# 定义一个字符串的月份和日期
+strMonth = ""
+strDay = ""
+
+# 定义一个索引，用于回溯
+index = 0
+remark = True
 # 今天的时间
-today =  datetime.datetime.now()
+today = datetime.datetime.now()
+while (remark):
 
-# 链接需要的参数
-todayParm = year + count + otherParm
+    if(len(str(today.month)) == 1):
+        strMonth = "0"+str(today.month)
+    else:
+        strMonth = str(today.month)
 
-# 爬起指定网址
-url = "https://www.cfmem.com/"+ftime()[0:4]+"/"+ftime()[4:6]+"/"+str(today.year)+str(today.month)+str(today.day)+"-"+todayParm.lower()+"-v2rayclash-vpn.html"
-res = requests.get(url,handlers)
-content = res.content
-html = etree.HTML(content)
+    if(len(str(today.day)) == 1):
+        strDay = "0"+str(today.day - index)
+    else:
+        strDay = str(today.day - index)
 
-# 数据解析
-for k in range(1,int(count)+1):
-    title = html.xpath('//*[@id="post-body"]/div[4]/pre/span['+str(k)+']')
-    for i in range(0,len(title)):
-        #处理元素获得节点
-        descText = title[i].xpath('string(.)')
-        # 将节点信息写入文件中
-        file.write(descText+"\n")
-# 关闭文件流        
+    # 链接需要的参数
+    todayParm = year + count + otherParm
+
+    # 爬起指定网址
+    url = "https://www.cfmem.com/"+ftime()[0:4]+"/"+ftime()[4:6]+"/"+str(
+        today.year)+strMonth+strDay+"-"+todayParm.lower()+"-v2rayclash-vpn.html"
+    res = requests.get(url, handlers)
+    content = res.content
+    html = etree.HTML(content)
+
+    # 数据解析
+    for k in range(1, int(count)+1):
+        title = html.xpath('//*[@id="post-body"]/div[4]/pre/span['+str(k)+']')
+        for i in range(0, len(title)):
+            # 处理元素获得节点
+            descText = title[i].xpath('string(.)')
+            if(descText != ''):
+                remark = False
+            # 将节点信息写入文件中
+            file.write(descText+"\n")
+    index = index + 1
+# 关闭文件流
 file.close()
